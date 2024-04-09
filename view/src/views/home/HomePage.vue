eeaf3bb9b077d6527c82208bb374f790ce0daac1
Popeyeeee111 <h0303250407@gmail.com>
2024-04-09 20:00:43 +0800

    首页完善

--- view/src/views/home/HomePage.vue
+++ view/src/views/home/HomePage.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>
-    <div class="background-image-container">
+  <div >
+    <div class="background-image-container" >
       <div>
         <div>
           <div>
-            <img src="../../assets/analyze.png" alt="First Image" class="image1" style="width: 200px; height: 200px; position: absolute; top: 500px; left: 350px;" @click="$router.push('search')">
-            <p style="position: absolute; top: 650px; left: 400px; font-size: 26px">分析局</p>
+            <img src="../../assets/222.png" alt="First Image" class="image1" style="width: 350px; height: 300px; position: absolute; top: 530px; left: 430px;" @click="$router.push('search')">
+            <p style="position: absolute; top: 800px; left: 550px; font-size: 26px">分析局</p>
           </div>
           <div>
-            <img src="@/assets/personal.png" alt="Second Image" class="image2" style="width: 200px; height: 200px; position: absolute; bottom: 200px; right: 240px" @click="$router.push('personal')">
-            <p style="position: absolute; bottom: 140px; right: 280px; font-size: 26px">个人中心</p>
+            <img src="@/assets/333.png" alt="Second Image" class="image2" style="width: 300px; height: 300px; position: absolute; bottom: 520px; right: 185px" @click="$router.push('personal')">
+            <p style="position: absolute; bottom: 505px; right:400px; font-size: 26px">个人中心</p>
+          </div>
+          <div>
+            <img src="@/assets/444.png" alt="Third Image" class="image3" style="width: 300px; height: 300px; position: absolute; bottom: 520px; left: 185px" >
           </div>
         </div>
       </div>
@@ -29,15 +32,16 @@ export default {
 <style scoped>
 .background-image-container {
   background-image: url('@/assets/background/homepage.png');
-  background-size: contain; /* 使背景图片完全显示在容器内 */
+  background-size: cover; /* 使用 cover 让背景图片填满整个容器 */
   background-position: center; /* 图片居中显示 */
-  height: 84vh; /* 使用视口高度作为容器高度 */
-  width: 84vw; /* 使用视口宽度作为容器宽度 */
+  height: 100vh; /* 使用视口高度作为容器高度 */
+  width: 100vw; /* 使用视口宽度作为容器宽度 */
   display: flex;
   justify-content: center;
   align-items: center;
   color: white; /* 文字颜色 */
 }
+
 
 /* 针对内容的额外样式 */
 .background-image-container > div {
